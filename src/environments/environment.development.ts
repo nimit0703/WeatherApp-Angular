c1f1cbd5232532b5013b7456f402ab2ed536587e
nimit0703 <nimit.190410107143@gmail.com>
2023-08-21 19:44:32 +0530

    fix image not showing error

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+    production:true,
+    weatherApiBaseUrl: 'https://weather-api99.p.rapidapi.com/weather' ,
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-api99.p.rapidapi.com',
+    XRapidAPIKeyHaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'61641f4642mshf1a3f4928049c87p1d3c64jsnc4fb4a400bf5'
+};
